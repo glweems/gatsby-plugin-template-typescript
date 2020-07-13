40fd7cb89e6b77ba02b9b03c1ceec105049003d3
Garrett Weems <gwgraphicdesign@gmail.com>
2020-07-13 14:30:16 -0500

    template updates

--- src/gatsby-node.ts
+++ src/gatsby-node.ts
@@ -1,8 +1,16 @@
-interface Options {
-    yourPluginOption: string;
+import { GatsbyNode, PluginOptions } from 'gatsby';
+
+export interface Options extends PluginOptions {
+  key: string;
 }
 
-export const sourceNodes = async (
-    { actions: { createNode }, store, createNodeId, createContentDigest, reporter }: GatsbyActions,
-    { yourPluginOption }: Options
+export const sourceNodes: GatsbyNode['sourceNodes'] = async (
+  {
+    actions: { createNode },
+    store,
+    createNodeId,
+    createContentDigest,
+    reporter,
+  },
+  options: Options
 ): Promise<void> => {};
